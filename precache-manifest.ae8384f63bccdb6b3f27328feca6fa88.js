5483d4aaf612ca78d87de4a0b4527947a16feba5
rahul250892 <rahulkumar250892@outlook.com>
2020-02-04 03:29:26 +0530

    Updates

---
+++ precache-manifest.ae8384f63bccdb6b3f27328feca6fa88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44180712f97de17b05e0",
+    "url": "/hello1/static/js/runtime~main.44180712.js"
+  },
+  {
+    "revision": "c40c669594e9c5801994",
+    "url": "/hello1/static/js/main.c40c6695.chunk.js"
+  },
+  {
+    "revision": "7b70c3a40d5c91ed34f5",
+    "url": "/hello1/static/js/1.7b70c3a4.chunk.js"
+  },
+  {
+    "revision": "c40c669594e9c5801994",
+    "url": "/hello1/static/css/main.f90a3b0e.chunk.css"
+  },
+  {
+    "revision": "c48deb23500d6d8770e5cff6ad9e2d16",
+    "url": "/hello1/index.html"
+  }
+];
